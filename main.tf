54ca027f1d5f25f7a3f41c2b3e7755c1e11b3e70
EC2 Default User <ec2-user@ip-172-31-23-90.ec2.internal>
2025-08-15 11:08:10 +0000

    test

---
+++ main.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+  region = "us-east-1" # change to your AWS region
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "terraform_lambda_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "${path.module}/lambda_function.py"
+  output_path = "${path.module}/lambda_function.zip"
+}
+
+resource "aws_lambda_function" "terraform_lambda" {
+  function_name = "terraform_hello_lambda"
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.9"
+
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
+  role = aws_iam_role.lambda_role.arn
+}
